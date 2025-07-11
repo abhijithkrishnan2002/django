1228faddbd9dd43b17ed30145a42816a0e04e7da
ABHIJITH KRISHNAN <abhijithkrishnan5657@gmail.com>
2025-07-11 16:15:05 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,10 @@
 from django.db import models
-
 class Login(models.Model):
     login_id=models.AutoField(primary_key=True)
     username=models.CharField(max_length=100)
     password=models.CharField(max_length=100)
     usertype=models.CharField(max_length=100)
     status=models.CharField(max_length=100)
-    
     class Meta:
         db_table= 'tbl_login'
 class UserInfo(models.Model):
@@ -15,5 +13,6 @@ class UserInfo(models.Model):
     address=models.TextField()
     phone_number=models.BigIntegerField(null=True)
     email=models.CharField(max_length=100,null=True)
+    login=models.ForeignKey(Login,blank=True,on_delete=models.CASCADE)
     class Meta:
         db_table= 'tbl_user'
